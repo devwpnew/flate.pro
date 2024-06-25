986cfd781e6ae181c0e28e738ab84ae7c2671c90
v3trovoy <v3trovoy@gmail.com>
2024-06-25 14:35:54 +0300

    fix:input number sum validation

--- @modules/common/components/input/inputRequaired.js
+++ @modules/common/components/input/inputRequaired.js
@@ -2,43 +2,54 @@ import { useEffect, useState } from "react";
 
 import Input from "./input";
 
-export default function InputRequired({ onChange, value, ...props }) {
-    const [isError, setIsError] = useState(null);
-    const handleErrorsOnChange = (ev) => {
-        setIsError(null);
+export default function InputRequired({
+  onChange,
+  onlyNumbers = false,
+  onlyNumbersBiggerThan = null,
+  value,
+  ...props
+}) {
+  const [isError, setIsError] = useState(null);
+  const handleErrorsOnChange = (ev) => {
+    setIsError(null);
 
-        if (ev.target.value === "") {
-            setIsError("Обязательно для заполнения");
-        }
+    if (ev.target.value === "") {
+      setIsError("Обязательно для заполнения");
+    }
 
-		if (parseInt(ev.target.value.replace(/\s/g, '')) < 300000) {
-            setIsError("Значение должно быть больше 300 000 ₽");
-        }
-    };
+    if (onlyNumbers && onlyNumbersBiggerThan) {
+      if (
+        parseInt(ev.target.value.replace(/\s/g, "")) < onlyNumbersBiggerThan
+      ) {
+        setIsError("Значение должно быть больше 300 000 ₽");
+      }
+    }
+  };
 
-    // if uncontrolled input
-    const onChangeHandler = (ev) => {
-        if (onChange) {
-            onChange(ev);
-        }
-        handleErrorsOnChange(ev);
-    };
+  // if uncontrolled input
+  const onChangeHandler = (ev) => {
+    if (onChange) {
+      onChange(ev);
+    }
+    handleErrorsOnChange(ev);
+  };
 
-    // if controlled input
-    useEffect(() => {
-        setIsError(null);
-        if (value === "") {
-            setIsError("Обязательно для заполнения");
-        }
-    }, [value]);
+  // if controlled input
+  useEffect(() => {
+    setIsError(null);
+    if (value === "") {
+      setIsError("Обязательно для заполнения");
+    }
+  }, [value]);
 
-    return (
-        <Input
-            onChange={onChangeHandler}
-            {...props}
-            isError={typeof isError === "string"}
-            errorMsg={isError}
-            value={value}
-        />
-    );
+  return (
+    <Input
+      onChange={onChangeHandler}
+      {...props}
+      onlyNumbers={onlyNumbers}
+      isError={typeof isError === "string"}
+      errorMsg={isError}
+      value={value}
+    />
+  );
 }
